b5ea4924094ed48e556193e8cea6036c15c759f4
love olubummo <olubummolove3@gmail.com>
2024-11-28 21:40:33 +0100

    update html

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const main = document.querySelector('main');
 const footer = document.querySelector('footer');
+const nav = document.querySelector('nav');
 const hamburgerContainer = document.querySelector('.ham');
 const hamburgerOpen = document.querySelector('.open');
 const hamburgerClose = document.querySelector('.close');
@@ -8,6 +9,14 @@ let checker = 'open';
 
 hamburgerContainer.addEventListener('click', ()=>{
     if (checker === 'open') {
-        
+        hamburgerOpen.style.display = 'none';
+        hamburgerClose.style.display = 'inline';
+        nav.style.display = 'flex';
+        main.style.display = 'none';
+        footer.style.display = 'none';
+        checker = 'close';
+    }
+    else {
+
     }
 })
