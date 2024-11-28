fdd813777082aebe33c5ee1ad1ae7e181a0b9418
love olubummo <olubummolove3@gmail.com>
2024-11-28 21:29:35 +0100

    update html

---
+++ script.js
@@ -0,0 +1,4 @@
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+const hamburgerOpen = document.querySelector('.open');
+const hamburgerClose = document.querySelector('.close');
