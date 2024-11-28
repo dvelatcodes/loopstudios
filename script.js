600dd3769a0566301c6935b933994a6646af54a8
love olubummo <olubummolove3@gmail.com>
2024-11-28 21:35:10 +0100

    update html

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const footer = document.querySelector('footer');
 const hamburgerContainer = document.querySelector('.ham');
 const hamburgerOpen = document.querySelector('.open');
 const hamburgerClose = document.querySelector('.close');
+let checker = 'open';
+
+
+hamburgerContainer.addEventListener('click', ()=>{
+    
+})
